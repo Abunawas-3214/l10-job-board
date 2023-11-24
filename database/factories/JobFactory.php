d780f5064e02eba9a0dc9f4c099949d591e37fc1
Abunawas <abunawas.arifandi@gmail.com>
2023-11-24 15:02:48 +0700

    course 102

--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Job;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -12,12 +13,17 @@ class JobFactory extends Factory
     /**
      * Define the model's default state.
      *
-     * @return array<string, mixed>
+     * @return array<string, mixed>6
      */
     public function definition(): array
     {
         return [
-            //
+            'title' => fake()->jobTitle,
+            'description' => fake()->paragraph(3, true),
+            'salary' => fake()->numberBetween(5_000, 150_000),
+            'location' => fake()->city,
+            'category' => fake()->randomElement(Job::$category),
+            'experience' => fake()->randomElement(Job::$exprience),
         ];
     }
 }
